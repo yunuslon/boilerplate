1c243ccd482e37e6f821095f38f493e35a7cc629
M. Yunus <myunus>
2023-08-20 10:52:26 +0700

    feat: add redux flipper

--- src/store/combinReducer.ts
+++ src/store/combinReducer.ts
@@ -2,17 +2,24 @@ import {
   legacy_createStore as createStore,
   applyMiddleware,
   combineReducers,
+  Middleware,
 } from 'redux';
 
 import ReduxThunk from 'redux-thunk';
 import GeneralReducer from './GeneralReducer/reducer';
 
+const middlewares: Middleware[] = [ReduxThunk];
+
+if (__DEV__) {
+  const createDebugger = require('redux-flipper').default;
+  middlewares.push(createDebugger());
+}
 const rootReducer = combineReducers({
   defaultReducer: () => [],
   generalReducer: GeneralReducer,
 });
 
 export default () => {
-  const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+  const store = createStore(rootReducer, applyMiddleware(...middlewares));
   return store;
 };
